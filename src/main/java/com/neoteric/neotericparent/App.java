ea2b7b98344ec206d3acf2fa581095c012d7ba9e
UDAY KUMAR <udaydevil2312@gmail.com>
2024-09-21 15:06:10 +0530

    add demo

---
+++ src/main/java/com/neoteric/neotericparent/App.java
@@ -0,0 +1,13 @@
+package com.neoteric.neotericparent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
